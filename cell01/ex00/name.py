e82c0ee44a7522342c07db8c68fd371dcebb0db3
wpooluei <Wuttiphatpooluei@gmail.com>
2025-06-18 03:45:51 +0000

    1.3

--- cell01/ex00/name.py
+++ cell01/ex00/name.py
@@ -1,3 +1,14 @@
-first_name="Aniwat"
-last_name="Inchamchoy"
-print(first_name,last_name)
+name= 'prawa'
+last_name='sayowan'
+
+s=f"Hey,what's your first name? :{name}"
+
+print(s)
+
+o=f"And your last name? : {last_name}"
+
+print(o)
+
+y=f"Well,pleased to meet you,{last_name}"
+
+print(y)
