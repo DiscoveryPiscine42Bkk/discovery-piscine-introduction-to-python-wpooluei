7018ec39303dd9588eb88cabc0a267209d9d1191
wpooluei <Wuttiphatpooluei@gmail.com>
2025-06-19 07:35:38 +0000

    fix cell02

---
+++ cell02/ex00/is_zero.py
@@ -0,0 +1,9 @@
+
+value = int(input())
+
+if value == 0:
+	print("This number is equal to zero")
+
+
+else:
+	print("This number is different from zero")
---
+++ cell02/ex02/password.py
@@ -0,0 +1,12 @@
+
+password = "Python is awesome"
+
+prompt = input()
+
+
+if prompt == password:
+	print("ACCESS GRANTED")
+
+
+else:
+	print("ACCESS DENIED")
