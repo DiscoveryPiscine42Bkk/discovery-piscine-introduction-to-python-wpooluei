24b75caa6a2b9eea42afcd04afc269e323d03871
wpooluei <Wuttiphatpooluei@gmail.com>
2025-06-20 06:32:16 +0000

    งานคอม

--- cell02/ex00/main.py checkmate.py
+++ cell02/ex00/main.py checkmate.py
@@ -1,13 +1,64 @@
-'''Main file'''
-from checkmate import checkmate
+def checkmate(board: str):
+  board = board.strip().split('\n')
+  size = len(board)
 
-def main():
-  board = """\
-....
-.K..
-....
-.R..\
-"""
-checkmate(board)
-if __name__ == "__main__":
-  main()
+  # is squre
+  for row in board:
+    if len(row) != size:
+      print("Error")
+      return
+    
+  # find King
+  king_pos = None
+  for i in range(size):
+    for j in range(len(board[i])):
+      if board[i][j] == 'K':
+        king_pos = (i, j)
+        break
+    if king_pos:
+      break
+  if not king_pos:
+    print("Error")
+    return
+  def in_bounds(x, y):
+    return 0 <= x < size and 0 <= y < len(board[x])
+  def is_pawn_attacking():
+    x, y = king_pos
+    for dx, dy in [(1, -1), (1, 1)]:
+      nx, ny = x + dx, y + dy
+      if in_bounds(nx, ny) and board[nx][ny] == 'P':
+          return True
+    return False
+  def is_bishop_attacking():
+    x, y = king_pos
+    for dx, dy in [(-1,-1), (-1,1), (1,-1), (1,1)]:
+      nx, ny = x + dx, y + dy
+      while in_bounds(nx, ny):
+        piece = board[nx][ny]
+        if piece == '.':
+            nx += dx
+            ny += dy
+        elif piece == 'B' or piece == 'Q':
+            return True
+        else:
+            break
+    return False
+  def is_rook_attacking():
+    x, y = king_pos
+    for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
+      nx, ny = x + dx, y + dy
+      while in_bounds(nx, ny):
+        piece = board[nx][ny]
+        if piece == '.':
+            nx += dx
+            ny += dy
+        elif piece == 'R' or piece == 'Q':
+            return True
+        else:
+          break
+    return False
+  
+  if is_pawn_attacking() or is_bishop_attacking() or is_rook_attacking():
+    print("Success")
+  else:
+    print("Fail")
